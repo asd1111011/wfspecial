ff306d7e1a0e3fd356feeb3357a4584fc58f65d0
1 <1@1.com>
2023-03-16 05:17:39 +0200

    a little custom

--- config.def.h
+++ config.def.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=15" };
+static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "󰣇", "", "󰙯", "", "", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -46,6 +46,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define OPTIONKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,16 +57,22 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-//static const char *dmenucmd[] = {"~/.scripts/powermenu", NULL};
+//static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = {"dmenu_run", "-c", "-l", "20" , NULL};
 static const char *termcmd[]  = { "st", NULL };
-static const char *powermenu[]  =  { "bash /home/caps/.scripts/powermenu" , NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_Escape, spawn,          SHCMD("~/.scripts/powermenu")},
+	{ MODKEY,                       XK_F4,     spawn,          SHCMD("pcmanfm")},
+	{ OPTIONKEY,                    XK_2,      spawn,          SHCMD("chromium")},
+	{ OPTIONKEY,                    XK_3,      spawn,          SHCMD("discord")},
+	{ OPTIONKEY,                    XK_4,      spawn,          SHCMD("sqlitebrowser")},
+	{ OPTIONKEY,                    XK_F1,     spawn,          SHCMD("setxkbmap us")},
+	{ OPTIONKEY,                    XK_F2,     spawn,          SHCMD("setxkbmap ua")},
+	{ OPTIONKEY,                    XK_F3,     spawn,          SHCMD("setxkbmap ru")},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
